5cfc9ac2eb3d3efd3fb95788fe5c53645b6d42a3
HyejinYang <yang94lol@gmail.com>
2020-07-10 18:04:57 +0900

    Trying to answer when users click a button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,73 +2,87 @@
 require 'json'
 
 BASE_URL = "https://slack.com/api/"
-TOKEN = 'xoxb-1226046072595-1234700942100-10fZCYVmR0yigwdfPBYwZTHV'
+TOKEN = 'xoxb-1226046072595-1234700942100-NpL8v6FBKYYXo0CSoh721R5P'
+
+GREETINGS=[
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "안녕하세요? 탁구봇 김탁구입니다. 탁구 내기와 관련된 건 kimtakoo를 불러주세요!"
+        }
+    },
+    {
+        "type": "divider"
+    },
+    {
+        "type": "actions",
+        "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "탁구 내기 결과 입력",
+                    "emoji": true
+                },
+                "value": "click_me_123",
+                "action_id": "action_takoo_match"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "탁구 내기 빚 보기",
+                    "emoji": true
+                },
+                "value": "click_me_12"
+            },
+        ]
+    }
+]
 
 class HomeController < ApplicationController
   def index
-    puts params
     if params.has_key?(:challenge)
       render json: params[:challenge]
     end
 
     if params[:event][:type] == "app_mention"
+
+      puts "="*20
+      puts JSON.pretty_generate(params)
+      puts "="*20
       msg = {:token => TOKEN,
-             :channel => 'announcements',
-             :text => params[:event][:text].as_json}
+             :channel => params[:event][:channel],
+             :text => params[:event][:text],
+             :blocks => GREETINGS.to_json
+      }
 
       req = Faraday.new
       res = req.post BASE_URL << 'chat.postMessage', msg
-    else
-      puts "params"
-      puts "=" * 20
-      puts params
-      puts "=" * 20
-      # params[:event][:type] == 'asfd'
-      blck = {
-          "type":"home",
-          "blocks":[
-              {
-                  "type":"section",
-                  "text":{
-                      "type":"mrkdwn",
-                      "text":"A simple stack of blocks for the simple sample Block Kit Home tab."
-                  }
-              },
-              {
-                  "type":"actions",
-                  "elements":[
-                      {
-                          "type":"button",
-                          "text":{
-                              "type":"plain_text",
-                              "text":"Action A",
-                              "emoji":true
-                          }
-                      },
-                      {
-                          "type":"button",
-                          "text":{
-                              "type":"plain_text",
-                              "text":"Action B",
-                              "emoji":true
-                          }
-                      }
-                  ]
-              }
-          ]
-      }
 
-      # msg = {:token => TOKEN,
-      #        :user_id => 'U0170DL7G81',
-      #        :views => blck.as_json}
-      #
-      # req = Faraday.new
-      # res = req.post BASE_URL << 'views.publish', msg
+      # begin
+      #   puts JSON.pretty_generate(JSON.parse(res.body))
+      # rescue
+      #   puts "Error occurred"
+      # end
     end
-    # begin
-    #   puts res.body
-    # rescue
-    #   puts "Error occurred"
-    # end
+
+
+  end
+
+  def bot
+    puts "="*20 + "버튼 눌렀을 때" + "="*20
+    payload_hash = JSON.parse(params[:payload])
+    puts JSON.pretty_generate(payload_hash)
+    # puts payload_hash
+    puts "="*40
+
+    msg = {"token" => 'xoxb-1226046072595-1234700942100-NpL8v6FBKYYXo0CSoh721R5P',
+           "channel" => "announcements",
+           "text" => 'ㄴㅁㅎㅇ',
+    }
+    req = Faraday.new
+    res = req.post BASE_URL << payload_hash["response_url"], msg
   end
 end
